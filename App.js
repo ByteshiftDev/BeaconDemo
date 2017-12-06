39c23f11cbd48409beae74d8ab51be70b7aae241
jasoncustodio <jmaxcustodio@gmail.com>
2017-12-06 10:01:40 -0800

    Changed Format

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react'
-import { AppRegistry, Text, View, DeviceEventEmitter } from 'react-native';
-import Beacons from 'react-native-beacons-manager';
-
+import React, { Component } from "react";
+import { AppRegistry, Text, View, DeviceEventEmitter } from "react-native";
+import Beacons from "react-native-beacons-manager";
 
 class App extends Component {
+  componentWillMount() {
+    Beacons.requestWhenInUseAuthorization();
+    Beacons.detectIBeacons();
+
+    // Monitoring
+    try {
+      const myRegion = {
+        identifier: "Test",
+        uuid: "01234567-0123-0123-0123-012345678910"
+      };
+
+      Beacons.startMonitoringForRegion(myRegion);
+
+      console.log("Beacons monitoring started successfully");
+    } catch (err) {
+      console.log("Beacons monitoring not started, error: ${err}");
+    }
+  }
+
+  componentDidMount() {
+    // monitoring:
+    DeviceEventEmitter.addListener("regionDidEnter", ({ identifier, uuid }) => {
+      console.log("monitoring - regionDidEnter data: ", { identifier, uuid });
+    });
+
+    DeviceEventEmitter.addListener("regionDidExit", ({ identifier, uuid }) => {
+      console.log("monitoring - regionDidExit data: ", { identifier, uuid });
+    });
+  }
+
   render() {
     return (
       <View>
@@ -13,44 +42,4 @@ class App extends Component {
   }
 }
 
-componentWillMount() {
-  Beacons.requestWhenInUseAuthorization();
-  Beacons.detectIBeacons();
-
-  // Monitoring
-  try {
-    const myRegion = {
-    identifier: 'Test',
-    uuid: '01234567-0123-0123-0123-012345678910',
-  };  
-
-  Beacons.startMonitoringForRegion(myRegion);
-
-  console.log('Beacons monitoring started successfully');
-  } catch (err) {
-  console.log('Beacons monitoring not started, error: ${err}');
-  }
-}
-
-componentDidMount() {
-  // monitoring:
-  DeviceEventEmitter.addListener(
-    'regionDidEnter',
-    ({ identifier, uuid }) => {
-      console.log('monitoring - regionDidEnter data: ', { identifier, uuid });
-    }
-  );
-
-  DeviceEventEmitter.addListener(
-    'regionDidExit',
-    ({ identifier, uuid }) => {
-      console.log('monitoring - regionDidExit data: ', { identifier, uuid });
-    }
-  );
-  
-}
-
-AppRegistry.registerComponent(
-  'App',
-  () => App
-);
+AppRegistry.registerComponent("App", () => App);
